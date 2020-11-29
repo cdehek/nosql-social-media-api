e648dcfe3d037add412b5310d95da61a0ca4a0e1
christian.dehek <christian.dehek@gmail.com>
2020-11-28 20:43:42 -0700

    setting up virtuals, also started on thought-controller.js

--- models/User.js
+++ models/User.js
@@ -4,19 +4,36 @@ const moment = require('moment');
 const UserSchema = new Schema(
     {
       username: {
-        type: String
+        type: String,
+        unique: true,
+        required: 'Please fill this out!'
       },
       email: {
-        type: String
+        type: String,
+        unique: true,
+        required: 'Please enter an email.',
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
       },
+      thoughts: [
+          {
+              type: Schema.Types.ObjectId,
+              ref: 'Thoughts'
+          }
+      ],
+      friends: [
+          {
+              type: Schema.Types.ObjectId,
+              ref: 'User'
+          }
+      ]
     },
-    // {
-    //   toJSON: {
-    //     virtuals: true,
-    //     getters: true
-    //   },
-    //   id: false
-    // }
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      id: false
+    }
   );
 
   // create User model using UserSchema
